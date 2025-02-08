e681de578ebbb7c3c43cd73c5b32ff3873310e76
David Grande <davidgrandewebs@gmail.com>
2025-02-08 20:44:54 +0100

    Fixed style problems map2

--- maps/script4.js
+++ maps/script4.js
@@ -1,5 +1,5 @@
 // 0. variables
-const apiLink2 = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nrg_cb_e?format=JSON&lang=EN&time=2022&nrg_bal=AFC";
+const apiLink2 = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nrg_cb_e?format=JSON&lang=EN&time=2022&nrg_bal=AFC&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=IS&geo=LI&geo=NO&geo=CH&geo=UK&geo=BA&geo=ME&geo=MK&geo=AL&geo=RS&geo=TR&geo=XK";
 
 const nodeListUseless = document.querySelectorAll(".pais2");
 const paisesMapa = [...nodeListUseless];
@@ -12,7 +12,7 @@ let datosJson;
 
 let paisObjetivo;
 let listaPaises = [];
-let consumicion;
+let consumicion = Array(42);
 
 //Fetch para recoger en arrays los datos del la consumición eléctrica de Eurostat
 const fetching = async () => {
@@ -24,31 +24,31 @@ const fetching = async () => {
         datosJson = await conexion.json();
 
         listaPaises = Object.entries(datosJson.dimension.geo.category.index).map(([siglas, id]) => ({ siglas, id }));
-
-        consumicion = Object.values(datosJson.value).slice(0, 42);
-        consumicion.splice(29, 0, "?");
-        consumicion.splice(32, 0, "?");
-        consumicion.splice(41, 0, "?");
-        
-        
-
+        consumicion = Object.values(datosJson.value);
         listaPaises.map((pais, index) => {
             pais.nombre = Object.values(datosJson.dimension.geo.category.label)[index];
 
-        })
+        });
+
+        listaPaises.splice(38, 0, { siglas: "UC", id: 38, nombre: "Ucrania" });
+        listaPaises.splice(39, 0, { siglas: "AN", id: 39, nombre: "Andorra" });
+        listaPaises.splice(40, 0, { siglas: "GR", id: 40, nombre: "Groenlandia" });
+        listaPaises.splice(41, 0, { siglas: "RU", id: 41, nombre: "Rusia" });
+        listaPaises.splice(42, 0, { siglas: "BY", id: 42, nombre: "Bielorrusia" });
+        listaPaises.splice(43, 0, { siglas: "CH", id: 43, nombre: "Suiza" });
+        listaPaises.splice(44, 0, { siglas: "MV", id: 44, nombre: "Moldavia" });
         //Poner consumicion en la listaPaises
         for (let w = 0, z = 0; w < listaPaises.length; w++) {
-            if (w === 29 || w === 30 || w === 32) {
+            if (w === 27 || w === 30 || w === 38 || w === 39 || w === 40 || w === 41 || w === 42 || w === 43 || w == 44) {
                 listaPaises[w].consumicion = "No hay datos sobre consumo eléctrico de este país en Eurostat";
             } else {
-                if (z == 29) {
+                /*if (z == 29) {
                     listaPaises[w].consumicion = consumicion[z + 1];
                     z = z + 2;
                 }
-                else {
-                    listaPaises[w].consumicion = consumicion[z];
-                    z++;
-                }
+                else {*/
+                listaPaises[w].consumicion = consumicion[z];
+                z++;
             }
         };
 
@@ -57,13 +57,14 @@ const fetching = async () => {
         paisesMapa.map(cadaUno => {
             cadaUno.addEventListener('mouseover', function (event) {
                 paisObjetivo = cadaUno.id;
+                console.log(paisObjetivo);
                 document.addEventListener("mousemove", function (event) {
                     // Obtener la posición actual del ratón
                     let x = event.clientX;
                     let y = event.clientY;
                     divFlotante.style.left = (x + 10) + "px";
                     divFlotante.style.top = (y + 10) + "px";
-                    if (paisObjetivo === "9" || paisObjetivo === "10" || paisObjetivo === "13" || paisObjetivo === "14" || paisObjetivo === "23" || paisObjetivo === "36" || paisObjetivo === "37" || paisObjetivo === "38" || paisObjetivo === "40" || paisObjetivo === "51") {
+                    if (paisObjetivo === "37" || paisObjetivo === "7" || paisObjetivo === "12" || paisObjetivo === "36" || paisObjetivo === "11" || paisObjetivo === "17" || paisObjetivo === "8") {
                         divFlotante.style.left = (x + 20) + "px";
                         divFlotante.style.top = (y - 130) + "px";
                     }
@@ -77,8 +78,8 @@ const fetching = async () => {
                 divFlotante.innerHTML = listaPaises.map((pais) => {
 
                     if (paisObjetivo === String(pais.id)) {
-                        return `<p><span class="categoria">País: </span>${pais.nombre}</br>
-            <span class="categoria">Consumo eléctrico en Gigavatios/hora (Gw/h): </br></span>${pais.consumicion}.</p>`;
+                        return `<p><span class="categoria"><strong>País: </strong></span>${pais.nombre}</br>
+            <span class="categoria"><strong>Consumo eléctrico(Gw/h):</strong> </br></span>${pais.consumicion}</p>`;
                     }
                     divFlotante.style.display = "block";
                     divFlotante.style.opacity = "0";
